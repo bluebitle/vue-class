291390dcbfc4ee07ecaa2384e78b8bb05da58e3e
daehyun_ryu <daehyun_ryu@kolon.com>
2020-07-13 17:17:47 +0900

    Initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>뷰는 재밋다.</div>
+    <router-link to="/">back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+.hello-css {
+  width: 100%
+}
+</style>
